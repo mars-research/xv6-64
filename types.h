f5be79fcfc8e145d4efeb2289db60e632f0e916f
GALL LEON <leon.gall@etu.unistra.fr>
2024-06-12 10:14:17 +0200

    feat: Add size-based types (e.g. uint64)

--- types.h
+++ types.h
@@ -1,4 +1,15 @@
 typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
-typedef uint pde_t;
+
+typedef unsigned char uint8;
+typedef signed char int8;
+
+typedef unsigned short int uint16;
+typedef signed short int int16;
+
+typedef unsigned int uint32;
+typedef signed int int32;
+
+typedef unsigned long long int uint64;
+typedef signed long long int int64;
