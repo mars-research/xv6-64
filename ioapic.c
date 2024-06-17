d7b5e164541093f375d51a58a82da157409eeecc
GALL LEON <leon.gall@etu.unistra.fr>
2024-06-17 14:23:48 +0200

    feat: Convert ioapicinit and dependencies to 64-bit

--- ioapic.c
+++ ioapic.c
@@ -5,6 +5,7 @@
 #include "types.h"
 #include "defs.h"
 #include "traps.h"
+#include "memlayout.h"
 
 #define IOAPIC  0xFEC00000   // Default physical address of IO APIC
 
@@ -50,7 +51,7 @@ ioapicinit(void)
 {
   int i, id, maxintr;
 
-  ioapic = (volatile struct ioapic*)IOAPIC;
+  ioapic = (volatile struct ioapic*)DEV_P2V(IOAPIC);
   maxintr = (ioapicread(REG_VER) >> 16) & 0xFF;
   id = ioapicread(REG_ID) >> 24;
   if(id != ioapicid)
